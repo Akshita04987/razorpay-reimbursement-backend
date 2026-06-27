1ad504f27a732e842034496db5586f32848e1130
Akshita Sharma <akshitash2005@gmail.com>
2026-06-27 12:09:15 +0530

    Fix cross origin cookies

--- Backend/src/controllers/authController.js
+++ Backend/src/controllers/authController.js
@@ -5,8 +5,8 @@ const env = require('../config/env');
 
 const cookieOptions = {
   httpOnly: true,
-  secure: env.nodeEnv === 'production',
-  sameSite: 'lax',
+  secure: true,
+  sameSite: 'none',
   maxAge: 24 * 60 * 60 * 1000
 };
 
